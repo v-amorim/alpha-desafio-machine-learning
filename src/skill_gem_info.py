1c446bf00b72724d16f725cdb7d445eafdf57cfd
Vinícius Amorim <vamorim.dev@gmail.com>
2023-12-14 23:39:12 -0300

    refactor: clean code and patterns

--- src/skill_gem_info.py
+++ src/skill_gem_info.py
@@ -1,83 +1,99 @@
 from __future__ import annotations
 
-from functools import cache
+import json
+from concurrent.futures import as_completed
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
 from lxml import html
-
-# URL to scrape
-main_url = 'https://www.poewiki.net/wiki/'
-list_of_skill_gems_url = main_url + 'List_of_skill_gems'
-
-# Dictionary of XPaths for skill gem names in different tables
-xpaths = {
-    'Red Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[2]/table/tbody/tr[*]/td[1]/span/span/a/text()',
-    'Green Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[3]/table/tbody/tr[*]/td[1]/span/span/a/text()',
-    'Blue Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[4]/table/tbody/tr[*]/td[1]/span/span/a/text()'
-}
-
-# List to store all skill gem names
-skill_gems = []
+from tqdm import tqdm
 
 
-@cache
-def get_parsed_tree(url):
-    # Define a caching decorator for the function that fetches and parses the HTML content
-    url_response = requests.get(url)
-    if url_response.status_code == 200:
-        return html.fromstring(url_response.content)
+class SkillGemScraper:
+    """Scrape skill gem details from Path of Exile wiki."""
 
-    print(f'Failed to retrieve the webpage. Status code: {url_response.status_code}')
-    return None
+    def __init__(self) -> None:
+        """Initialize the SkillGemScraper."""
+        self.base_url: str = 'https://www.poewiki.net/wiki/'
+        self.skill_gems: list[str] = []
 
+    def get_parsed_tree(self, url: str) -> html.HtmlElement | None:
+        """Fetch and parse HTML content."""
+        response = requests.get(url)
+        if response.status_code == 200:
+            return html.fromstring(response.content)
 
-# Iterate through each entry in the dictionary
-for category, xpath in xpaths.items():
-    # Use the cached function to get the parsed HTML tree
-    tree = get_parsed_tree(list_of_skill_gems_url)
-
-    if tree is not None:
-        # Extract skill gem names using the current XPath and extend the list
-        skill_gems.extend(tree.xpath(xpath))
-
-# Print the list of all skill gem names
-print('Skill Gems:', skill_gems)
-
-
-# URL to scrape
-for skill_gem in skill_gems:
-    skill_gem_url = main_url + skill_gem
-
-    # XPath for the desired main text
-    skill_gem_description_xpath = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[2]/span[3]/text()'
-
-    # XPath for the desired titles (all <a> elements)
-    skill_gem_tags_xpath = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[2]/span[1]/a/text()'
-
-    # XPath for additional text
-    skill_gem_name_xpath = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[1]/text()'
-
-    # Send a GET request to the URL
-    response = requests.get(skill_gem_url)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the HTML content
-        tree = html.fromstring(response.content)
-
-        # Extract the main text using XPath
-        skill_gem_description = tree.xpath(skill_gem_description_xpath)
-
-        # Extract all titles using XPath and store them in a list
-        skill_gem_tags = tree.xpath(skill_gem_tags_xpath)
-
-        # Extract additional text using XPath
-        skill_gem_name = tree.xpath(skill_gem_name_xpath)
-
-        # Print the extracted main text, titles, and additional text
-        print('Skill Gem:', skill_gem_name)
-        print('Skill Gem Tags:', skill_gem_tags)
-        print('Skill Gem Description:', skill_gem_description)
-
-    else:
         print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
+        return None
+
+    def scrape_skill_gem_names(self) -> None:
+        """Scrape skill gem names from different categories."""
+        category_xpaths: dict[str, str] = {
+            'Red Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[2]/table/tbody/tr[*]/td[1]/span/span/a/text()',
+            'Green Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[3]/table/tbody/tr[*]/td[1]/span/span/a/text()',
+            'Blue Skill Gems': '//*[@id="mw-content-text"]/div[1]/div[4]/table/tbody/tr[*]/td[1]/span/span/a/text()'
+        }
+
+        for xpath in category_xpaths.values():
+            tree = self.get_parsed_tree(self.base_url + 'List_of_skill_gems')
+            if tree is not None:
+                self.skill_gems.extend(tree.xpath(xpath))
+
+    def scrape_skill_gem_details(self, skill_gem: str) -> dict[str, list[str]] | None:
+        """Scrape details for a given skill gem."""
+        skill_gem_url: str = self.base_url + skill_gem
+        description_xpath: str = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[2]/span[3]/text()'
+        tags_xpath: str = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[2]/span[1]/a/text()'
+        name_xpath: str = '//*[@id="mw-content-text"]/div[1]/p[1]/span/span[1]/span[1]/text()'
+
+        response = requests.get(skill_gem_url)
+
+        if response.status_code == 200:
+            tree = html.fromstring(response.content)
+            description: list[str] = tree.xpath(description_xpath)
+            tags: list[str] = tree.xpath(tags_xpath)
+            name: list[str] = tree.xpath(name_xpath)
+
+            return {
+                'Skill Gem': name,
+                'Skill Gem Tags': tags,
+                'Skill Gem Description': description
+            }
+
+        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
+        return None
+
+    def scrape_all_skill_gem_details(self) -> list[dict[str, list[str]]]:
+        """Scrape details for all skill gems using multithreading."""
+        self.scrape_skill_gem_names()
+
+        with ThreadPoolExecutor() as executor, tqdm(
+                total=len(self.skill_gems), desc='Scraping'
+        ) as pbar:
+            futures = {
+                executor.submit(self.scrape_skill_gem_details, skill_gem): skill_gem for skill_gem in self.skill_gems}
+
+            for _ in as_completed(futures):
+                pbar.update(1)
+
+            results: list[dict[str, list[str]] | None] = [future.result() for future in futures]
+
+        # Filter out None results (failed requests)
+        results = [result for result in results if result is not None]
+
+        return results
+
+
+if __name__ == '__main__':
+    # Create an instance of the class and perform the scraping
+    scraper: SkillGemScraper = SkillGemScraper()
+    skill_gem_details: list[dict[str, list[str]]] = scraper.scrape_all_skill_gem_details()
+
+    # Convert the result to JSON
+    result_json: str = json.dumps(skill_gem_details, indent=2)
+
+    # Save the JSON to a file
+    with open('skill_gem_details.json', 'w', encoding='utf8') as json_file:
+        json_file.write(result_json + '\n')
+
+    print("Scraping completed. Result saved to 'skill_gem_details.json'")
