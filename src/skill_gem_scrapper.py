8373077c84d6bb7f42deea78013ae3bc9b88c887
Gusta <qsuamae@gmail.com>
2023-12-21 13:23:39 -0300

    adição de mais uma formatação.

--- src/skill_gem_scrapper.py
+++ src/skill_gem_scrapper.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 import json
 import os
-from concurrent.futures import as_completed
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import console_colors as cc
 import requests
@@ -38,6 +37,7 @@ class SkillGemScraper:
         self.base_xpath: str = '//*[@id="mw-content-text"]/div[1]'
         self.skill_gems: list[str] = []
         self.skill_gem_color: str = skill_gem_color
+        self.session = requests.Session()
 
     def format_skill_gem_details(self, skill_gem_name, skill_gem_tags, skill_gem_description):
         formatted_tags = ', '.join(skill_gem_tags)
@@ -51,7 +51,7 @@ class SkillGemScraper:
 
     def get_parsed_tree(self, url: str) -> html.HtmlElement | None:
         """Fetch and parse HTML content."""
-        response = requests.get(url)
+        response = self.session.get(url)
         if response.status_code == 200:
             return html.fromstring(response.content)
 
@@ -67,15 +67,15 @@ class SkillGemScraper:
         }
 
         if self.skill_gem_color == 'All Skill Gems':
-            xpaths = category_xpaths.values()
+            xpaths = list(category_xpaths.values())
         elif self.skill_gem_color in category_xpaths:
             xpaths = [category_xpaths[self.skill_gem_color]]
         else:
             raise ValueError(f'Invalid color: {self.skill_gem_color}')
 
-        for xpath in xpaths:
-            tree = self.get_parsed_tree(self.base_url + 'List_of_skill_gems')
-            if tree is not None:
+        tree = self.get_parsed_tree(self.base_url + 'List_of_skill_gems')
+        if tree is not None:
+            for xpath in xpaths:
                 self.skill_gems.extend(tree.xpath(xpath))
 
     def get_non_empty_xpath(self, tree, xpaths):
@@ -110,7 +110,7 @@ class SkillGemScraper:
             f'{self.base_xpath}/div[3]/span[1]/span[2]/span[@class="group tc -gemdesc"]/text()',
         ]
 
-        response = requests.get(skill_gem_url)
+        response = self.session.get(skill_gem_url)
 
         if response.status_code == 200:
             tree = html.fromstring(response.content)
@@ -134,20 +134,26 @@ class SkillGemScraper:
         with ThreadPoolExecutor() as executor, tqdm(
                 total=len(self.skill_gems), desc='Scraping', colour='green'
         ) as pbar:
-            results = []
+            futures = {
+                executor.submit(self.scrape_skill_gem_details, skill_gem): skill_gem for skill_gem in self.skill_gems
+            }
 
-            for skill_gem in self.skill_gems:
-                result = self.scrape_skill_gem_details(skill_gem)
+            formatted_results = []
+            original_results = []
+            for future in as_completed(futures):
+                result = future.result()
                 if result:
                     formatted_details = self.format_skill_gem_details(
                         result['Skill Gem'][0],
                         result['Skill Gem Tags'],
                         result['Skill Gem Description']
                     )
-                    results.append(formatted_details)
+                    formatted_results.append(formatted_details)
+                    original_results.append(result)
+
                 pbar.update(1)
 
-        return results
+        return formatted_results, original_results
 
 
 def main():
@@ -162,19 +168,24 @@ def main():
     selected_color = UserInterface.get_user_choice(skill_gem_colors)
 
     scraper = SkillGemScraper(skill_gem_color=selected_color)
-    skill_gem_details = scraper.scrape_all_skill_gem_details()
+    formatted_results, original_results = scraper.scrape_all_skill_gem_details()
 
     result_folder_name = 'skill_gem_data'
-    result_json_path = f'{result_folder_name}/skill_gem_details.json'
+    formatted_json_path = f'{result_folder_name}/skill_gems_treated.json'
+    original_json_path = f'{result_folder_name}/skill_gem_details.json'
 
     # Check if the folder exists, and create it if not
     if not os.path.exists(result_folder_name):
         os.makedirs(result_folder_name)
 
-    with open(result_json_path, 'w', encoding='utf8') as json_file:
-        json.dump(skill_gem_details, json_file, indent=2)
+    with open(formatted_json_path, 'w', encoding='utf8') as formatted_json_file:
+        json.dump(formatted_results, formatted_json_file, indent=2)
 
-    print(f'{cc.green_done()} Scraping completed. Result saved to {cc.blue_var(result_json_path)}')
+    with open(original_json_path, 'w', encoding='utf8') as original_json_file:
+        json.dump(original_results, original_json_file, indent=2)
+
+    print(f'{cc.green_done()} Scraping completed. Result saved to '
+          f'{cc.blue_var(formatted_json_path)} and {cc.blue_var(original_json_path)}')
 
 
 if __name__ == '__main__':
