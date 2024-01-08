d47541d380e325b905a22e42b38dada1c78ca136
Lucas Nogueira Policano <loki1336doki@gmail.com>
2024-01-08 10:04:07 -0300

    feat: Ligação front-api

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from fastapi import FastAPI
+from fastapi import HTTPException
 from fastapi import Request
 from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-# from fastapi import HTTPException
-# from fastapi.responses import JSONResponse
 # from src.reranking import rerank
 
 app = FastAPI()
@@ -14,23 +14,25 @@ templates = Jinja2Templates(directory='src/app/templates')
 app.mount('/static', StaticFiles(directory='src/app/static'), name='static')
 
 
-# @app.post('/receive-input')
-# async def receive_input(request: Request):
-# try:
-# data = await request.json()
-# query = data.get('query')
-# if not query:
-# raise HTTPException(status_code=400, detail='Query não fornecida')
+@app.post('/recive-input')
+async def receive_input(request: Request):
+    try:
+        data = await request.json()
+        user_input = data.get('userInput')
+        if not user_input:
+            raise HTTPException(status_code=400, detail='Input do usuário não fornecido')
 
-# reranked_results = rerank(query)
+        print(f'Received user input: {user_input}')
 
-# resposta_gerada = 'oi'
+        # Substitua a lógica abaixo com o processamento real da entrada do usuário
+        resposta_gerada = 'Resposta gerada pela API'
 
-# except Exception as e:
-# raise HTTPException(status_code=500, detail=f'Erro ao processar a entrada do usuário: {str(e)}') from e
+    except Exception as e:
+        print(f'Erro ao processar a entrada do usuário: {str(e)}')
+        raise HTTPException(status_code=500, detail=f'Erro ao processar a entrada do usuário: {str(e)}') from e
 
-# if reranked_results:
-# return JSONResponse(content={'message': 'Input recebido com sucesso!', 'resposta': resposta_gerada})
+    if resposta_gerada:
+        return JSONResponse(content={'message': 'Input recebido com sucesso!', 'resposta': resposta_gerada})
 
 
 @app.get('/', response_class=HTMLResponse)
